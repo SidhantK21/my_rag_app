b4a03223b5ecb8d73f492dcf0caae69c6aca29e5
Sidhant Singh Rathore <sidhantsinghrathore@Sidhants-MacBook-Air.local>
2025-03-30 18:59:32 +0530

    Refreshed .gitignore

--- backend/dist/prousers/pro.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const proRouter = (0, express_1.Router)();
-proRouter.post("/prosignup", async (req, res) => {
-});
